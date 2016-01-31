ee1b4ab1dc5d58fca38c637654007b6458f09bbc
Adam Bien <abien@adam-bien.com>
2016-01-31 15:03:49 +0100

    Retention fixed to Runtime

--- jcache-javaee/src/main/java/javax/enterprise/cache/CacheContext.java
+++ jcache-javaee/src/main/java/javax/enterprise/cache/CacheContext.java
@@ -2,6 +2,8 @@ package javax.enterprise.cache;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -9,6 +11,7 @@ import java.lang.annotation.Target;
  * @author airhacks.com
  */
 @Documented
+@Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.METHOD})
 public @interface CacheContext {
 
